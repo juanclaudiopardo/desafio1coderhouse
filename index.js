2e272c5886f0ef48a4ffef197dc5b7867954013e
Juan Pardo <josefrecinosanitarios@yahoo.com.ar>
2022-04-17 18:36:52 -0300

    desafio 1

---
+++ index.js
@@ -0,0 +1,13 @@
+//Ciclo For
+const numero = +prompt("Ingrese la cantidad de holas que quiere ver por consola:");
+for (let i = 0; i < numero; i++) {
+  console.log("Hola!!");
+}
+//Dados(ciclo Whileg)
+let dado = Math.trunc(Math.random() * 6) + 1;
+
+while (dado !== 6) {
+  console.log(`Te salio ${dado}`);
+  dado = Math.trunc(Math.random() * 6) + 1;
+  if (dado === 6) console.log("Se termina el juego");
+}
